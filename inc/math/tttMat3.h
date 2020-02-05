80c0368f66e820fd5cef45f1e59df279b0fd01fb
Alexander Orel <desperius@gmail.com>
2020-02-05 12:37:10 +0200

    Matrix and vector multiplications were added

--- inc/math/tttMat3.h
+++ inc/math/tttMat3.h
@@ -42,6 +42,24 @@ const typename tmat3<T>::col_type& tmat3<T>::operator[](tmat3<T>::length_type i)
     return this->value[i];
 }
 
+template<class T>
+typename tmat3<T>::col_type operator*(const tmat3<T>& m, const typename tmat3<T>::row_type& v)
+{
+    return typename tmat3<T>::col_type(
+        m[0][0] * v.x + m[1][0] * v.y + m[2][0] * v.z,
+        m[0][1] * v.x + m[1][1] * v.y + m[2][1] * v.z,
+        m[0][2] * v.x + m[1][2] * v.y + m[2][2] * v.z);
+}
+
+template<class T>
+typename tmat3<T>::row_type operator*(const typename tmat3<T>::col_type& v, const tmat3<T>& m)
+{
+    return typename tmat3<T>::row_type(
+        v.x * m[0][0] + v.y * m[0][1] + v.z * m[0][2],
+        v.x * m[1][0] + v.y * m[1][1] + v.z * m[1][2],
+        v.x * m[2][0] + v.y * m[2][1] + v.z * m[2][2]);
+}
+
 typedef tmat3<float> mat3;
     
 } /* namespace ttt */
